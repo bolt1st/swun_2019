c9d65460c24e4383bd2f06f8a643863ebce624e9
unknown <wangjipeng@imagedesign.com>
2018-09-07 18:29:06 +0800

    1

--- keras_extend_dataset.py
+++ keras_extend_dataset.py
@@ -7,20 +7,20 @@ from keras.preprocessing import image
 
 
 datagen = ImageDataGenerator(
-        rotation_range=40,            # 随机旋转的度数范围
+        rotation_range=60,            # 随机旋转的度数范围
         width_shift_range=0.2,
         height_shift_range=0.2,
         rescale=1.0/255,               # 重缩放因子(数据乘以所提供的值)
-        shear_range=0.2,              # 剪切强度（以弧度逆时针方向剪切角度）
-        zoom_range=0.2,               # 随机缩放范围
+        shear_range=0.1,              # 剪切强度（以弧度逆时针方向剪切角度）
+        zoom_range=0.1,               # 随机缩放范围
         horizontal_flip=True,         # 随机水平翻转
         fill_mode='nearest'           # {"constant", "nearest", "reflect" or "wrap"} 之一。默认为 'nearest'。输入边界以外的点根据给定的模式填充
 )
 
-train_corn_dir = 'C:\\Users\\data\\train\\leaf_blight'
+train_corn_dir = 'C:\\Users\\wangjipeng.IMAGEDESIGN\\Desktop\\开题相关\\data\\玉米大斑病'
 fname = [os.path.join(train_corn_dir, fname) for fname in os.listdir(train_corn_dir)]
 # 选择一个图像
-img_path = fname[0]
+img_path = fname[1]
 # 读取图片并调整大小为150*150
 # img = Image.open(img_path)
 # img1 = img.Image.resize(150, 150)
@@ -32,9 +32,9 @@ x = x.reshape((1,) + x.shape)
 
 # 下面是产生图片的代码，产生的图片保存在‘train’目录下
 i = 0
-for batch in datagen.flow(x, batch_size=1, save_to_dir='train', save_prefix='corn', save_format='jpg'):
+for batch in datagen.flow(x, batch_size=1, save_to_dir='train\\leaf_blight', save_prefix='leaf_blight', save_format='jpg'):
         i += 1
-        if i > 50:
+        if i > 49:
                 break
 
 
